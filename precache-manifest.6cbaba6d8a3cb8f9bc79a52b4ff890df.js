b1c67f217de534ae41b31b968f88f56d6c2b2f9a
Vaibhav Kapoor <vaibhavkapoor.480@gmail.com>
2023-03-27 21:04:05 +0530

    Updates

---
+++ precache-manifest.6cbaba6d8a3cb8f9bc79a52b4ff890df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2490a5588d931990c48045b514312918",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "3a3e2da9a06386ac23dd",
+    "url": "/Keeper-App/static/js/2.140c2324.chunk.js"
+  },
+  {
+    "revision": "53235b01857aac86a881bed9870e171f",
+    "url": "/Keeper-App/static/js/2.140c2324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94098d5673dc46d76ad7",
+    "url": "/Keeper-App/static/js/main.4061b09b.chunk.js"
+  },
+  {
+    "revision": "44a50b49fc7d570e2380",
+    "url": "/Keeper-App/static/js/runtime-main.3a2a4e55.js"
+  }
+]);
